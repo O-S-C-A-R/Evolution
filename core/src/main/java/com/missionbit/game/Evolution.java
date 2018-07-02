16bee689af26da16594fe361303fea33e3d51394
Trent TaylorIII”git config --global user.name Trent <iii3rdtaylor@gmail.comgit config --global user.email iii3rdtaylor@gmail.com>
2018-07-02 11:56:58 -0700

    White background

--- core/src/main/java/com/missionbit/game/Evolution.java
+++ core/src/main/java/com/missionbit/game/Evolution.java
@@ -1,7 +1,57 @@
 package com.missionbit.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+import java.util.Random;
+
 public class Evolution extends ApplicationAdapter {
+    private OrthographicCamera camera;
+    private Random randomSource;
+    private Sprite myImage;
+    private SpriteBatch myBatch;
+    private Vector2 velocity;
+
+    @Override
+    public void create() {
+        randomSource = new Random();
+
+        // Set up camera for 2d view of 800x480 pixels
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        // Create a sprite batch for rendering our image
+        myBatch = new SpriteBatch();
+
+        //TODO: Load our image
+    }
+
+    @Override
+    public void render() {
+
+        // Clear the screen
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Set up our camera
+        camera.update();
+        myBatch.setProjectionMatrix(camera.combined);
+
+        //TODO: Draw our image!
+    }
+
+    @Override
+    public void dispose() {
+        myBatch.dispose();
+    }
 }
