350bad8103871bfba6b5022db92e1f5b4e6bdb8d
Trent TaylorIII”git config --global user.name Trent <iii3rdtaylor@gmail.comgit config --global user.email iii3rdtaylor@gmail.com>
2018-07-05 13:59:26 -0700

    lol

--- core/src/main/java/com/missionbit/game/Evolution.java
+++ core/src/main/java/com/missionbit/game/Evolution.java
@@ -37,18 +37,15 @@ public class Evolution extends ApplicationAdapter {
     private SpriteBatch myBatch;
     //private Vector2 velocity;
     private float Speed;
-    private boolean showDebug =true;
+    private boolean showDebug =false;
     private boolean touchplatform = true;
     private Vector2 lastposition = new Vector2();
-<<<<<<< HEAD
 
     private Buttons LeftButton;
 
 
+ private boolean platformcheck = false;
 
-=======
-    private boolean platformcheck = false;
->>>>>>> fb26c9f47d0211634957e229f6b31587639a8ecd
     private static final int[][] PLAT_LOCS = new int[][] {
             {0, 0, 1500, 60}, // floor
             {390,60,78,28},
