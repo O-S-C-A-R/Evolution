794756faabe7c7952fa6e37dc1fae55bf1af57e6
Trent TaylorIII”git config --global user.name Trent <iii3rdtaylor@gmail.comgit config --global user.email iii3rdtaylor@gmail.com>
2018-07-04 18:15:03 -0700

    UPDATES

--- core/src/main/java/com/missionbit/game/Enemies.java
+++ core/src/main/java/com/missionbit/game/Enemies.java
@@ -15,17 +15,17 @@ public class Enemies {
     private int direction = -1;
     //private int otherdirection = 5;
     private float speed = 1;
+
     public Enemies(float x, float y) {
-        images = new Sprite( new Texture(Gdx.files.internal("images/Spider.png")));
+        images = new Sprite(new Texture(Gdx.files.internal("images/Spider.png")));
         images.setX(x);
         images.setY(y);
 
     }
-   public void draw(SpriteBatch batch){
-       images.setX(images.getX() + speed * direction);
-     //  images.setX(images.getX() + speed * otherdirection);
-       images.draw(batch);
 
-   }
-
+    public void draw(SpriteBatch batch) {
+        images.setX(images.getX() + speed * direction);
+        //  images.setX(images.getX() + speed * otherdirection);
+        images.draw(batch);
+    }
 }
