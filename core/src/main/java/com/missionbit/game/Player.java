ab8479206a47de362e462d20147518091fc3a8ef
Trent Taylor III <iii3rdtaylor@gmail.com>
2018-07-10 09:04:13 -0700

    lol

--- core/src/main/java/com/missionbit/game/Player.java
+++ core/src/main/java/com/missionbit/game/Player.java
@@ -30,7 +30,6 @@ public class Player {
 
     public Player(){
         BlackPlayer = new Sprite( new Texture(Gdx.files.internal("images/player/BlackPlayer.png")));
-
         BlackPlayer.setX(70);
         BlackPlayer.setY(59);
     }
