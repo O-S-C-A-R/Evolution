476b48ce7fee207e8d0e8b655bac3df8145bf766
flybanana <aaronchacon96@gmail.com>
2018-07-17 12:27:32 -0700

    gg

--- core/src/main/java/com/missionbit/game/LASER.java
+++ core/src/main/java/com/missionbit/game/LASER.java
@@ -1,4 +1,30 @@
 package com.missionbit.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class LASER {
-}
+    private ParticleEffect effect;
+
+ public LASER(float x, float y){
+     effect = new ParticleEffect();
+     effect.load(Gdx.files.internal("images/particle effects/Laser"), Gdx.files.internal("images/particle effects"));
+     effect.setPosition(x, y);
+     effect.start();
+ }
+     public void draw(SpriteBatch myBatch){
+         effect.draw(myBatch, Gdx.graphics.getDeltaTime());
+
+
+     }
+
+
+
+//             effect.setPosition(myImage.getX() + myImage.getWidth() / 2.0f, myImage.getY() + myImage.getHeight() / 2.0f);
+//
+//                 effect.start();
+//
+//
+//            effect.draw(myBatch, Gdx.graphics.getDeltaTime());
+        }
