2dc019ecd1648a43738882bedc8ced31365a207c
Trent Taylor III <iii3rdtaylor@gmail.com>
2018-07-18 10:18:28 -0700

    fix

--- core/src/main/java/com/missionbit/game/States/LevelOne.java
+++ core/src/main/java/com/missionbit/game/States/LevelOne.java
@@ -1,19 +1,4 @@
 package com.missionbit.game.States;
 public class LevelOne {
-//
-//    private static final int[][] PLAT_LOCS = new int[][]{
-//            {0, 0, 1800, 60}, // PLATFORMS
-//            {390, 60, 78, 28},
-//            {545, 60, 84, 75},
-//            {754, 160, 235, 5},
-//    };
-//    private static final float[][] spike_locs = new float[][]{
-//            {755, 59, 880, 125, 1000, 59},// SPIKES
-//            {1359, 61,1450,100,1600, 235, 1800, 235, 1800, 61},
-//    };
-}
-
-//        Teast = new LASER(8, 20);
 //private LASER Teast;
-//Teast.draw(myBatch);
-
+}
