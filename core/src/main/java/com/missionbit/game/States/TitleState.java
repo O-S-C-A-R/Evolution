ec2f3cdd6b5e114cc22ad8d2504b16f6ebaf6c54
Trent Taylor III <iii3rdtaylor@gmail.com>
2018-07-18 13:52:15 -0700

    errors and delay time.

--- core/src/main/java/com/missionbit/game/States/TitleState.java
+++ core/src/main/java/com/missionbit/game/States/TitleState.java
@@ -14,15 +14,10 @@ public class TitleState extends State
     public Texture TitleScreen;
     public Buttons Start;
     public Vector3 touchPos;
-<<<<<<< HEAD
 
     private Rumble rumble;
     private long Time;
 
-=======
-    private Rumble rumble;
-    private long Time;
->>>>>>> 610a4e077033d4a2e7884c5ed06c19f8857d5f2b
     private Music music;
     public TitleState(GameStateManager gsm)
     {
