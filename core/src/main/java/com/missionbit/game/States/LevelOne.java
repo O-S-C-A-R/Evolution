af034f36f0fc055e7c299a4c568efd385c046f05
Oscar Tiong <Ostiong@s.sfusd.edu>
2018-07-18 11:37:37 -0700

    rumble

--- core/src/main/java/com/missionbit/game/States/LevelOne.java
+++ core/src/main/java/com/missionbit/game/States/LevelOne.java
@@ -1,6 +1,6 @@
 package com.missionbit.game.States;
 public class LevelOne {
-<<<<<<< HEAD
+
 //
 //    private static final int[][] PLAT_LOCS = new int[][]{
 //            {0, 0, 1800, 60}, // PLATFORMS
@@ -16,6 +16,4 @@ public class LevelOne {
         //Teast = new LASER(8, 20);
 //private LASER Teast;
 //        Teast.draw(myBatch);
-=======
-}
->>>>>>> 00cf603940ce0ca5b43404511d8671a62f14a2e4
+
--- core/src/main/java/com/missionbit/game/States/TitleState.java
+++ core/src/main/java/com/missionbit/game/States/TitleState.java
@@ -6,19 +6,25 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.missionbit.game.Buttons;
 import com.missionbit.game.Evolution;
+import com.missionbit.game.Rumble;
 
 public class TitleState extends State
 {
     public Texture TitleScreen;
     public Buttons Start;
     public Vector3 touchPos;
+    private Rumble rumble;
+    private long Time;
 
     public TitleState(GameStateManager gsm)
     {
         super(gsm);
+        Time = System.currentTimeMillis();
         TitleScreen = new Texture(Gdx.files.internal("images/ui/TitleScreen.png"));
         Start = new Buttons(400,44, "images/ui/Start.png");
+        rumble = new Rumble();
         cam.setToOrtho(false, Evolution.VIEWPORT_WIDTH, Evolution.VIEWPORT_HEIGHT);
+
     }
 
     protected void handleInput()
@@ -46,10 +52,34 @@ public class TitleState extends State
     }
     public void render(SpriteBatch sb)
     {
+
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
         sb.draw(TitleScreen, 0, 0);
         sb.end();
+
+//        for (int i = 100; i < System.currentTimeMillis() - Time; i+=100){
+            rumble.rumble(2f, 3f);
+            if (Rumble.getRumbleTimeLeft() > 0) {
+                Rumble.tick(Gdx.graphics.getDeltaTime());
+                cam.translate(Rumble.getPos());
+            }
+            Time = System.currentTimeMillis();
+
+        cam.update();
+//        }
+//        if (System.currentTimeMillis() - Time > 500) {
+//            rumble.rumble(2f, 1f);
+//            if (Rumble.getRumbleTimeLeft() > 0) {
+//                Rumble.tick(Gdx.graphics.getDeltaTime());
+//                cam.translate(Rumble.getPos());
+//            }
+//            Time = System.currentTimeMillis();
+//        }
+//        cam.update();
+//        else {
+//            rumble.setCurrentPower(0f);
+//        }
     }
     public void dispose()
     {
