c9944ea34328e6a84e1330078c6f10e58398ddf7
Trent Taylor III <iii3rdtaylor@gmail.com>
2018-07-23 11:06:45 -0700

    AHHAHAHAH

--- core/src/main/java/com/missionbit/game/States/TitleState.java
+++ core/src/main/java/com/missionbit/game/States/TitleState.java
@@ -53,7 +53,7 @@ public class TitleState extends State
             if (Start.HandleClick(touchPos))
             {
 
-                gsm.set(new LevelTwo(gsm));
+                gsm.set(new TutorialState(gsm));
                 Levelmaker.GameMode = true;
             }
 
